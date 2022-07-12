a8d3cf0f27f4d56e1bbca64394dd2072c92c1e54
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-12 22:27:43 +0530

    guestCustomer Controller

---
+++ src/test/java/lk/ijse/spring/service/GuestCustomerServiceTest.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuestCustomerServiceTest {
+
+    @Test
+    void saveGuestCustomer() {
+    }
+
+    @Test
+    void deleteGuestCustomer() {
+    }
+
+    @Test
+    void updateGuestCustomer() {
+    }
+
+    @Test
+    void searchGuestCustomer() {
+    }
+
+    @Test
+    void getAllGuestCustomer() {
+    }
+}
