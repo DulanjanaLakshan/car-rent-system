f376add78921e62998c5840160661a499df375cd
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-12 23:33:44 +0530

    EmployeeControllerler Registered Customer Controller ok

--- src/main/java/lk/ijse/spring/controller/RegisteredCustomerController.java
+++ src/main/java/lk/ijse/spring/controller/RegisteredCustomerController.java
@@ -1,8 +1,7 @@
 package lk.ijse.spring.controller;
 
-
-import lk.ijse.spring.dto.GuestCustomerDTO;
-import lk.ijse.spring.service.GuestCustomerService;
+import lk.ijse.spring.dto.RegisteredCustomerDTO;
+import lk.ijse.spring.service.RegisteredCustomerService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,38 +9,39 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("guest/customer")
+@RequestMapping("registered/customer")
 @CrossOrigin
-public class GuestCustomerController {
+public class RegisteredCustomerController {
+
     @Autowired
-    GuestCustomerService customerService;
+    RegisteredCustomerService customer;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil getAllGuestCustomer() {
-        return new ResponseUtil(200, "ok", customerService.getAllGuestCustomer());
+    public ResponseUtil getAllCustomers() {
+        return new ResponseUtil(200, "Ok", customer.getAllRegisteredCustomer());
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil saveCustomer(@ModelAttribute GuestCustomerDTO customer) {
-        customerService.saveGuestCustomer(customer);
-        return new ResponseUtil(200, "save", null);
+    public ResponseUtil saveCustomer(@ModelAttribute RegisteredCustomerDTO customerDTO) {
+        customer.saveRegisteredCustomer(customerDTO);
+        return new ResponseUtil(200, "Save", null);
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil updateCustomer(@RequestBody GuestCustomerDTO customer) {
-        customerService.updateGuestCustomer(customer);
+    public ResponseUtil updateCustomer(@RequestBody RegisteredCustomerDTO customerDTO) {
+        customer.updateRegisteredCustomer(customerDTO);
         return new ResponseUtil(200, "Updated", null);
     }
 
     @DeleteMapping(params = {"id"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil deleteCustomer(@RequestParam String id) {
-        customerService.deleteGuestCustomer(id);
+        customer.deleteRegisteredCustomer(id);
         return new ResponseUtil(200, "Deleted", null);
     }
 
     @GetMapping(path = "{/id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil searchCustomer(@PathVariable String id) {
-        return new ResponseUtil(200, "Ok", customerService.searchGuestCustomer(id));
+        return new ResponseUtil(200, "Ok", customer.searchRegisteredCustomer(id));
     }
 }
