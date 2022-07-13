9c9670bd350e3b296fb198af11af08e0691f1e4e
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-13 10:12:25 +0530

    Drivers Controller ok

---
+++ src/main/java/lk/ijse/spring/controller/DriversController.java
@@ -0,0 +1,46 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.DriverService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("drivers")
+@CrossOrigin
+public class DriversController {
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllDrivers(){
+        return new ResponseUtil(200,"Ok",driverService.getAllDrivers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveDrivers(@ModelAttribute DriverDTO driverDTO){
+        driverService.saveDrivers(driverDTO);
+        return new ResponseUtil(200,"Save",null);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateDrivers(@RequestBody DriverDTO driverDTO){
+        driverService.updateDrivers(driverDTO);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteDrivers(@RequestParam String id){
+        driverService.deleteDrivers(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchDrivers(@PathVariable String id){
+        return new ResponseUtil(200,"Ok",driverService.searchDrivers(id));
+    }
+}
