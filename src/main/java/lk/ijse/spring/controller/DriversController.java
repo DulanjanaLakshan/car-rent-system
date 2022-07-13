f8432c00864177db8d851827b1192a520a9e6417
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-13 19:31:15 +0530

    vehical Controller Ok

--- src/main/java/lk/ijse/spring/controller/DriversController.java
+++ src/main/java/lk/ijse/spring/controller/DriversController.java
@@ -1,7 +1,6 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.DriverDTO;
-import lk.ijse.spring.dto.EmployeeDTO;
 import lk.ijse.spring.service.DriverService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/main/java/lk/ijse/spring/controller/VehicleController.java
@@ -0,0 +1,48 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.VehicleDTO;
+import lk.ijse.spring.service.VehicleService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("vehicle")
+@CrossOrigin
+public class VehicleController {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllVehicle() {
+        return new ResponseUtil(200, "Ok", vehicleService.getAllVehicle());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED) //201
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveVehicle(@ModelAttribute VehicleDTO vehicleDTO) {
+        System.out.println(vehicleDTO);
+        vehicleService.saveVehicle(vehicleDTO);
+        return new ResponseUtil(200, "Save", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        vehicleService.updateVehicle(vehicleDTO);
+        return new ResponseUtil(200, "Updated", null);
+    }
+
+    @DeleteMapping(params = {"id"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteVehicle(@RequestParam String id) {
+        vehicleService.deleteVehicle(id);
+        return new ResponseUtil(200, "Deleted", null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchVehicle(@PathVariable String id){
+        return new ResponseUtil(200,"Ok",vehicleService.searchVehicle(id));
+    }
+}
